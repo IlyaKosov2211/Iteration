73a07148dc1e19bb1ed1c69f997d9fb3eb07e2de
Илья <ilakosov8@gmail.com>
2023-04-04 23:46:31 +0700

    Second commit

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,15 +1,9 @@
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
-import java.util.Random;
-
-
 public class Randoms implements Iterable<Integer> {
-    protected PrimitiveIterator.OfInt random;
     protected int min;
     protected int max;
 
     public Randoms(int min, int max) {
-        random = new Random().ints(min, (max + 1)).iterator();
         this.min = min;
         this.max = max;
     }
@@ -24,7 +18,7 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                return random.nextInt();
+                return min + (int) (Math.random() * ((max - min) + 1));
             }
         };
     }
